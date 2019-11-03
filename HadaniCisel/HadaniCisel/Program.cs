d295a0d05245fde3f137a92e1a35d2757c445898
Robert Haken <haken@havit.cz>
2019-11-03 23:22:09 +0100

    HadaniCisel

---
+++ HadaniCisel/HadaniCisel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HadaniCisel
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int horniMez = 1000;
+			Console.WriteLine($"Myslím si číslo od 0 do {horniMez}." +
+				$" Uhádneš, které to je?");
+
+			int cislo = new Random().Next(1001);
+			int tipovaneCislo;
+			int pocetTipu = 0;
+
+			do
+			{
+				tipovaneCislo = Convert.ToInt32(Console.ReadLine());
+				pocetTipu = pocetTipu + 1;
+				if (tipovaneCislo > cislo)
+				{
+					Console.WriteLine("To není ono! Zkus menší číslo...");
+				}
+				if (tipovaneCislo < cislo)
+				{
+					Console.WriteLine("To není ono! Zkus větší číslo...");
+				}
+			}
+			while (tipovaneCislo != cislo);
+
+			Console.WriteLine($"To je ono! Gratuluji," +
+				$" uhádl(a) si číslo na {pocetTipu}. pokus.");
+		}
+	}
+}
