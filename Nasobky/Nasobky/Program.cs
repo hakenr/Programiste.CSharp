779519c0bfb5e4db292f57f46431b7563e5be807
Robert Haken <haken@havit.cz>
2019-11-24 22:53:53 +0100

    Nasobky

---
+++ Nasobky/Nasobky/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Nasobky
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine($"Vypíšu násobky A, které jsou zároveň násobky B.");
+			Console.WriteLine("Zadejte celé číslo A:");
+			int a = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Zadejte celé číslo B:");
+			int b = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine($"Násobky {a}, které jsou zároveň násobky {b}:");
+			int nasobek = 0;
+			for (int poradi = 0; poradi < 10; poradi++)
+			{
+				do
+				{
+					nasobek = nasobek + a;
+				}
+				while ((nasobek % b) != 0); // % vypočte zbytek po dělení
+				Console.WriteLine(nasobek);
+			}
+		}
+	}
+}
