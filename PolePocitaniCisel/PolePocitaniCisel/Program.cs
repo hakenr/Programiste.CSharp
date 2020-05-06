31c678de083af18ea2a478c6781766a77bb30e89
Robert Haken <haken@havit.cz>
2020-05-07 01:51:26 +0200

    PolePocitaniCisel

---
+++ PolePocitaniCisel/PolePocitaniCisel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PolePocitaniCisel
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Zadej hausnumero:");
+			var vstup = Console.ReadLine();
+
+			int[] cetnostCislic = new int[10];
+
+			foreach (var prvek in vstup)
+			{
+				var cislice = Convert.ToInt32(prvek.ToString());
+				cetnostCislic[cislice] = cetnostCislic[cislice] + 1;
+			}
+
+			for (int i = 0; i < cetnostCislic.Length; i++)
+			{
+				Console.WriteLine($"Číslice {i} je tam {cetnostCislic[i]}x.");
+			}
+		}
+	}
+}
