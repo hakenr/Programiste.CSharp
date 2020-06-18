bedff9e6510cbe1445c96ec3a832a0946535abb8
Robert Haken <haken@havit.cz>
2020-06-18 16:06:03 +0200

    SQL injection

---
+++ SqlInjectionDemo/SqlInjectionDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Data.SqlClient;
+
+namespace SqlInjectionDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Username:");
+			var username = Console.ReadLine();
+
+			Console.WriteLine("Password:");
+			var password = Console.ReadLine();
+
+			// SELECT UserID FROM User WHERE Username='pepa' AND Password = 'hesllo'
+			string cmdText = "SELECT UserId FROM [User] WHERE Username=@username AND Password=@password";
+			Console.WriteLine(cmdText);
+
+
+			using var conn = new SqlConnection("Server=rhdemosql.database.windows.net;Database=AdventureWorksLT;User Id=PrgStudent;Password=Karel je borec.;");
+			conn.Open();
+			using var cmd = new SqlCommand(cmdText, conn);
+			cmd.Parameters.AddWithValue("username", username);
+			cmd.Parameters.AddWithValue("password", password);
+
+			var userID = cmd.ExecuteScalar();
+			Console.Write("ID přihlášeného uživatele:");
+			Console.WriteLine(userID);
+		}
+	}
+}
---
+++ WebDownloader/WebDownloader/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace WebDownloader
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			HttpClient client = new HttpClient();
+			string stranka = client.GetStringAsync("https://www.mensagymnazium.cz/cs/kontakty").Result;
+
+			Regex re = new Regex(@"<span class=""name"">(?<jmeno>.*)</span>");
+			foreach (Match match in re.Matches(stranka))
+			{
+				Console.WriteLine(match.Groups["jmeno"]);
+			}
+
+			//Console.WriteLine(stranka);
+		}
+	}
+}
+
+//			HttpRequestMessage request = new HttpRequestMessage();
+//			request.Method = HttpMethod.Get;
+//			request.Headers.Add("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.44");
+//			request.RequestUri = new Uri("https://www.mensagymnazium.cz/cs/kontakty");
+
+//			using var response2 = client.SendAsync(request).Result;
+//			var statusCode = response2.StatusCode;
+//// 			var header = response2.Headers.GetValues("Content-Type");
+//			var stranka = response2.Content.ReadAsStringAsync().Result;
