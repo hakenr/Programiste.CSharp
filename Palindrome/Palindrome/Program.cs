0b88cb5502e7177bc86f8ec030b7c4461f993014
Robert Haken <haken@havit.cz>
2021-12-02 11:28:58 +0100

    Palindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+Console.WriteLine("Zadejte vstupní text, najdu v něm palindromy.");
+var input = Console.ReadLine();
+
+Console.WriteLine("\nNalezené palindromy:");
+var words = input.Split(' ');
+foreach (var word in words)
+{
+	string wordTrimmed = word.Trim(',', '.', '?', '!', ':');
+	char[] chars = wordTrimmed.ToCharArray();
+	Array.Reverse(chars);
+	string reversedWord = new string(chars);	
+	if (String.Equals(wordTrimmed, reversedWord, StringComparison.OrdinalIgnoreCase))
+	{
+		Console.WriteLine(wordTrimmed);
+	}
+}
