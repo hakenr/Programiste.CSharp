15f90cbf9b0998dd0acf372a38f6edd4cd8fdc17
Robert Haken <haken@havit.cz>
2022-01-06 10:39:36 +0100

    RecursionMaxDepth

---
+++ RecursionMaxDepth/RecursionMaxDepth/Program.cs
@@ -0,0 +1,20 @@
+﻿long counter = 0;
+
+void MyMethod()
+{
+	// increase stack-frame size
+	//Int64 a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p;
+
+	counter++;
+
+	Console.WriteLine(counter.ToString());
+
+	MyMethod();
+}
+
+MyMethod();
+
+// increase stack size
+//var thread = new Thread(MyMethod, int.MaxValue);
+//thread.Start();
+//thread.Join();
