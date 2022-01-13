aabc301daba51afaa3cb39c0602ae3314aec6448
Robert Haken <haken@havit.cz>
2022-01-13 11:48:36 +0100

    BankingOop

---
+++ BankingOop/BankingOop/Program.cs
@@ -0,0 +1,109 @@
+﻿var account1 = new BankAccount("Adam", 0);
+Console.WriteLine($"Account {account1.Number} was created for {account1.Owner} with {account1.Balance} initial balance.");
+
+account1.MakeDeposit(500, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account1.Balance);
+
+account1.MakeWithdrawal(100, DateTime.Now, "Rent payment");
+Console.WriteLine(account1.Balance);
+
+Console.WriteLine(account1.GetAccountHistory());
+
+
+var account2 = new BankAccount("David", 100);
+Console.WriteLine($"Account {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
+
+account2.MakeDeposit(200, DateTime.Now, "Gift from parents");
+account2.MakeWithdrawal(30, DateTime.Now, "Games subscription");
+account2.MakeDeposit(1800, DateTime.Now, "Salary");
+
+Console.WriteLine(account2.GetAccountHistory());
+
+
+public class BankAccount
+{
+	private static int accountNumberSeed = 1000000;
+	public string Number { get; }
+	public string Owner { get; set; }
+	public decimal Balance
+	{
+		get
+		{
+			decimal balance = 0;
+			foreach (var item in allTransactions)
+			{
+				balance += item.Amount;
+			}
+
+			return balance;
+		}
+	}
+
+	private List<Transaction> allTransactions = new List<Transaction>();
+
+	public BankAccount(string name, decimal initialBalance)
+	{
+		this.Number = accountNumberSeed.ToString();
+		accountNumberSeed++;
+
+		this.Owner = name;
+
+		if (initialBalance != 0)
+		{
+			MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+		}
+	}
+
+	public void MakeDeposit(decimal amount, DateTime date, string note)
+	{
+		if (amount <= 0)
+		{
+			throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+		}
+		var deposit = new Transaction(amount, date, note);
+		allTransactions.Add(deposit);
+	}
+
+	public void MakeWithdrawal(decimal amount, DateTime date, string note)
+	{
+		if (amount <= 0)
+		{
+			throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+		}
+		if (Balance - amount < 0)
+		{
+			throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+		}
+		var withdrawal = new Transaction(-amount, date, note);
+		allTransactions.Add(withdrawal);
+	}
+
+	public string GetAccountHistory()
+	{
+		var report = new System.Text.StringBuilder();
+
+		decimal balance = 0;
+		report.AppendLine("Date\t\tAmount\tBalance\tNote");
+		foreach (var item in allTransactions)
+		{
+			balance += item.Amount;
+			report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+		}
+
+		return report.ToString();
+	}
+}
+
+public class Transaction
+{
+	public decimal Amount { get; }
+	public DateTime Date { get; }
+	public string Notes { get; }
+
+	public Transaction(decimal amount, DateTime date, string note)
+	{
+		this.Amount = amount;
+		this.Date = date;
+		this.Notes = note;
+	}
+}
---
+++ BankingOop/README.md
@@ -0,0 +1,29 @@
+﻿# Bankovní účet (třídy)
+
+Procvičíme si základní práci s třídami (`class`) - jejich definicí a použitím.
+
+Definujte objektový model (třídy), který bude reprezentovat základní bankovnictví:
+* Bankovní účet má vlastníka (jméno) a počáteční vklad (částku).
+* Bankovní účet má číslo účtu, které je mu automaticky přiděleno při založení.
+* Změny zůstatku bankovního účtu provádíme prostřednictvím transakcí na něm. Každý účet si pamatuje seznam transakcí, které se s ním prováděly.
+  * Každá transakce má datum, popis a částku (kladnou/zápornou).
+* Bankovní účet podporuje několik operací
+  * Vklad na účet - Musí být kladný, vytvoří transakci zvyšující zůstatek.
+  * Výběr z účtu - Musí být kladný, vytvoří transakci snižující zůstatek. Nelze provést, pokud zůstatek účtu nepostačuje na požadovanou částku.
+  * Počáteční vklad - Vzniká při založení účtu a je v podstatě první transakcí na účtu.
+  * Výpis z účtu - Vrátí textovou reprezentaci seznamu všech transakcí s účtem.
+
+*Poznámka: Model bankovnictvím je zde pro výukové účely značně zjednodušen. Ve skutečném bankovnictvím má například každá transakce dvě strany - dva účty, mezi kterými se transakce provádí.*
+
+![Screenshot](screenshot.png)
+
+## Challenges (volitelná rozšíření zadání)
+1. Reprezentujte transakce jako operace mezi dvěma účty.
+2. Přidejte podporu převodu peněz mezi dvěma účty. Hlídejte podmínky, které musí účty splňovat, aby bylo možné transakci provést.
+3. Přidejte podporu termínovaných spořících účtů. Takový účet nedovolí provádět transakce mimo zvolené periodické datum - např. jen v jeden konkrétní den v týdnu (týdenní fixace), nebo měsíci (měsíční fixace).
+4. Přidejte podporu úročení účtů. Na každém účtu bude evidována úroková sazba a nějakou centrální úlohu spouštěnou jednou měsíčně připište na účty získané úroky.
+
+## Inspirace
+
+Obdobnou úlohu s podrobným popisem řešení a zdrojovými kódy najdete v souvisejícím kurzu:
+https://docs.microsoft.com/cs-cz/dotnet/csharp/fundamentals/tutorials/classes
