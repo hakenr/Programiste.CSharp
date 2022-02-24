5a76058a68632a9a0fba89c27dccd6c4bbb2b428
Robert Haken <haken@havit.cz>
2022-02-24 11:24:40 +0100

    RomanNumerals - římské číslice

---
+++ RomanNumerals/RomanNumerals/Program.cs
@@ -0,0 +1,56 @@
+﻿var calculator = new RomanNumeralsCalculator();
+
+Console.WriteLine(calculator.RomanToArabic("I")); // 1
+Console.WriteLine(calculator.RomanToArabic("VIII")); // 8
+Console.WriteLine(calculator.RomanToArabic("XXI")); // 21
+Console.WriteLine(calculator.RomanToArabic("CLXXIII")); // 173
+Console.WriteLine(calculator.RomanToArabic("MDCLXVI")); // 1666
+Console.WriteLine(calculator.RomanToArabic("MDCCCXXII")); // 1822
+Console.WriteLine(calculator.RomanToArabic("MCMLIV")); // 1954
+Console.WriteLine(calculator.RomanToArabic("MCMXC")); // 1990
+Console.WriteLine(calculator.RomanToArabic("MMVIII")); // 2008
+Console.WriteLine(calculator.RomanToArabic("MMXIV")); // 2014
+
+
+public class RomanNumeralsCalculator
+{
+	private Dictionary<char, int> charValues;
+
+	public RomanNumeralsCalculator()
+	{
+		charValues = new Dictionary<char, int>();
+		charValues.Add('I', 1);
+		charValues.Add('V', 5);
+		charValues.Add('X', 10);
+		charValues.Add('L', 50);
+		charValues.Add('C', 100);
+		charValues.Add('D', 500);
+		charValues.Add('M', 1000);
+	}
+
+	public int RomanToArabic(string roman)
+	{
+		if (roman.Length == 0) return 0;
+		roman = roman.ToUpper();
+
+		int total = 0;
+		int lastValue = 0;
+		for (int i = roman.Length - 1; i >= 0; i--)
+		{
+			int new_value = charValues[roman[i]];
+
+			// přičítáme nebo odečítáme?
+			if (new_value < lastValue)
+			{
+				total -= new_value;
+			}
+			else
+			{
+				total += new_value;
+				lastValue = new_value;
+			}
+		}
+
+		return total;
+	}
+}
