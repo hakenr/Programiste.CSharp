8d175fbdedaad6b1ed4fd820e221e4286aba355b
Robert Haken <haken@havit.cz>
2023-01-05 11:24:16 +0100

    DirectionsReduction (template)

---
+++ DirectionsReduction/DirectionsReduction/DirReductionTests.cs
@@ -0,0 +1,22 @@
+namespace DirReduction
+{
+	[TestClass]
+	public class DirReductionTests
+	{
+		[TestMethod]
+		public void Test1()
+		{
+			string[] a = new string[] { "NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST" };
+			string[] b = new string[] { "WEST" };
+			Assert.AreEqual(b, DirReduction.dirReduc(a));
+		}
+		
+		[TestMethod]
+		public void Test2()
+		{
+			string[] a = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+			string[] b = new string[] { "NORTH", "WEST", "SOUTH", "EAST" };
+			Assert.AreEqual(b, DirReduction.dirReduc(a));
+		}
+	}
+}
---
+++ DirectionsReduction/DirectionsReduction/DireReductions.cs
@@ -0,0 +1,13 @@
+﻿#pragma warning disable IDE1006 // Naming Styles - CodeWars nedodržuje konvence pojmenování pro C#
+
+namespace DirReduction
+{
+	public class DirReduction
+	{
+		public static string[] dirReduc(string[] arr)
+		{
+			// TODO Your code goes here.
+			return null;
+		}
+	}
+}
