0c2140a1ee43b549f8e2e8e55a35aac2d6940597
Robert Haken <haken@havit.cz>
2023-10-17 23:25:01 +0200

    HadaniCisel net7

--- HadaniCisel/HadaniCisel/Program.cs
+++ HadaniCisel/HadaniCisel/Program.cs
@@ -1,36 +1,25 @@
-﻿using System;
+﻿int horniMez = 1000;
+Console.WriteLine($"Myslím si číslo od 0 do {horniMez}." +
+	$" Uhádneš, které to je?");
 
-namespace HadaniCisel
+int cislo = new Random().Next(horniMez + 1);
+int tipovaneCislo;
+int pocetTipu = 0;
+
+do
 {
-	class Program
+	tipovaneCislo = Convert.ToInt32(Console.ReadLine());
+	pocetTipu = pocetTipu + 1;
+	if (tipovaneCislo > cislo)
 	{
-		static void Main(string[] args)
-		{
-			int horniMez = 1000;
-			Console.WriteLine($"Myslím si číslo od 0 do {horniMez}." +
-				$" Uhádneš, které to je?");
-
-			int cislo = new Random().Next(horniMez + 1);
-			int tipovaneCislo;
-			int pocetTipu = 0;
-
-			do
-			{
-				tipovaneCislo = Convert.ToInt32(Console.ReadLine());
-				pocetTipu = pocetTipu + 1;
-				if (tipovaneCislo > cislo)
-				{
-					Console.WriteLine("To není ono! Zkus menší číslo...");
-				}
-				if (tipovaneCislo < cislo)
-				{
-					Console.WriteLine("To není ono! Zkus větší číslo...");
-				}
-			}
-			while (tipovaneCislo != cislo);
-
-			Console.WriteLine($"To je ono! Gratuluji," +
-				$" uhádl(a) si číslo na {pocetTipu}. pokus.");
-		}
+		Console.WriteLine("To není ono! Zkus menší číslo...");
+	}
+	if (tipovaneCislo < cislo)
+	{
+		Console.WriteLine("To není ono! Zkus větší číslo...");
 	}
 }
+while (tipovaneCislo != cislo);
+
+Console.WriteLine($"To je ono! Gratuluji," +
+	$" uhádl(a) si číslo na {pocetTipu}. pokus.");
