348660439e2ee28e704f3ff8d1ef277edb5ea28e
Robert Haken <haken@havit.cz>
2023-10-30 20:57:13 +0100

    PolePocitaniCisel - net7 + zadání

--- PolePocitaniCisel/PolePocitaniCisel/Program.cs
+++ PolePocitaniCisel/PolePocitaniCisel/Program.cs
@@ -1,26 +1,15 @@
-﻿using System;
+﻿Console.Write("Zadej hausnumero:");
+var vstup = Console.ReadLine();
 
-namespace PolePocitaniCisel
+int[] cetnostCislic = new int[10];
+
+foreach (var prvek in vstup)
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.Write("Zadej hausnumero:");
-			var vstup = Console.ReadLine();
+	var cislice = Convert.ToInt32(prvek.ToString());
+	cetnostCislic[cislice] = cetnostCislic[cislice] + 1;
+}
 
-			int[] cetnostCislic = new int[10];
-
-			foreach (var prvek in vstup)
-			{
-				var cislice = Convert.ToInt32(prvek.ToString());
-				cetnostCislic[cislice] = cetnostCislic[cislice] + 1;
-			}
-
-			for (int i = 0; i < cetnostCislic.Length; i++)
-			{
-				Console.WriteLine($"Číslice {i} je tam {cetnostCislic[i]}x.");
-			}
-		}
-	}
+for (int i = 0; i < cetnostCislic.Length; i++)
+{
+	Console.WriteLine($"Číslice {i} je tam {cetnostCislic[i]}x.");
 }
