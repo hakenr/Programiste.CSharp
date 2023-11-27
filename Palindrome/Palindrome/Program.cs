e6dea5bec5e07c7b9db50a1d1ebdecc8666b1745
Robert Haken <haken@havit.cz>
2023-11-27 23:28:30 +0100

    Palindrome - net7 + zadání DOCX+PDF

--- Palindrome/Palindrome/Program.cs
+++ Palindrome/Palindrome/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-Console.WriteLine("Zadejte vstupní text, najdu v něm palindromy.");
+﻿Console.WriteLine("Zadejte vstupní text, najdu v něm palindromy.");
 var input = Console.ReadLine();
 
 Console.WriteLine("\nNalezené palindromy:");
@@ -10,7 +8,7 @@ foreach (var word in words)
 	string wordTrimmed = word.Trim(',', '.', '?', '!', ':');
 	char[] chars = wordTrimmed.ToCharArray();
 	Array.Reverse(chars);
-	string reversedWord = new string(chars);	
+	string reversedWord = new string(chars);
 	if (String.Equals(wordTrimmed, reversedWord, StringComparison.OrdinalIgnoreCase))
 	{
 		Console.WriteLine(wordTrimmed);
